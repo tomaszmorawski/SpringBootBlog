0242005c729e9d2883ce86d251e363183f7c2662
Tomasz Mórawski <tomasz.morawski1987@gmail.com>
2019-06-19 21:46:53 +0200

    adding role "USER" to new user

--- src/main/java/me/spiochu/blog/Services/UserService.java
+++ src/main/java/me/spiochu/blog/Services/UserService.java
@@ -1,18 +1,23 @@
 package me.spiochu.blog.Services;
 
 import me.spiochu.blog.model.User;
+import me.spiochu.blog.repository.RoleRepository;
 import me.spiochu.blog.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     UserRepository userRepository;
+    RoleRepository roleRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     public void addUser(User newUser) {
+        newUser.getRoles().add(roleRepository.getOne(1L));
         userRepository.save(newUser);
+
     }
 }
